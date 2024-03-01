3de5d273d4a94fd4a4a413f836864b21939ded07
Janek2004 <jan.zagorski04@gmail.com>
2024-03-01 21:15:03 +0100

    Update env loading process

--- socketio/index.js
+++ socketio/index.js
@@ -1,7 +1,20 @@
 const admin = require('firebase-admin');
 const { Server } = require('socket.io');
 
-const serviceAccount = JSON.parse(process.env['firebase']);
+const serviceAccount = {
+    "type": "service_account",
+    "project_id": process.env['project_id'],
+    "private_key_id": process.env['private_key_id'],
+    "private_key": process.env['private_key'],
+    "client_email": process.env['client_email'],
+    "client_id": process.env['client_id'],
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": process.env['client_x509_cert_url'],
+    "universe_domain": "googleapis.com"
+}
+
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 
 const io = new Server({});
