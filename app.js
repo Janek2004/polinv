ce54bc1fe923fb43062545e25050b25dc7d6ceb2
Janek2004 <jan.zagorski04@gmail.com>
2024-03-03 00:30:54 +0100

    Major update - Working :smiley:

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ const http = require("http");
 const server = http.createServer(app);
 
 const { Server } = require('socket.io');
-const io = new Server(server, { cors: { origin: '*' } });
+const io = new Server(server);
 
 require('./socketio')(io);
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+
+    res.send('Hi 😊');
+
+});
 
 const port = 3000;
 server.listen(port, () => {
--- socketio/index.js
+++ socketio/index.js
@@ -26,20 +26,44 @@ function verify_device(socket) {
 
 }
 
+function check_token(socket) { }
+
+function on_connection(socket) {
+
+    console.log(`New socket connected (${socket.id}) -> ${socket.email}`);
+
+    socket.on('cmd', (device_id, action) => {
+        const validDevice = devices.get(socket.uid)?.get(device_id);
+        if (validDevice) socket.to(device_id).emit(action);
+    });
+
+    socket.on('get devices', () => {
+        const user_devices = devices.get(socket.uid);
+        socket.emit('devices', user_devices);
+    });
+
+    socket.on('disconnect', reason => {
+        console.log(`Socket disconnected (${socket.id}) -> ${reason}`);
+        if (socket.isDevice) {
+            devices.get(socket.uid).delete(socket.id);
+            const user_devices = devices.get(socket.uid);
+            io.to(socket.uid).emit('devices', user_devices);
+        }
+    });
+
+}
+
 module.exports = io => {
 
     io.use((socket, next) => {
 
-        console.log('...');
-
         const token = socket.handshake.auth.token;
-        if (typeof token !== 'string' || !token) next(new Error('Invalid token'));
+        if (typeof token !== 'string' || !token) return next(new Error('Invalid token'));
 
         admin.auth().verifyIdToken(token).then(decodedToken => {
             const uid = decodedToken.uid;
             const email = decodedToken.email;
             socket.uid = uid;
-            socket.join(uid);
             socket.email = email;
             if (!devices.get(socket.uid)) devices.set(socket.uid, new Map());
             next();
@@ -50,28 +74,12 @@ module.exports = io => {
     io.use((socket, next) => {
 
         socket.join(socket.id);
+        socket.join(socket.uid);
         if (socket.handshake.auth.device) verify_device(socket);
         next();
 
     });
 
-    io.on('connection', socket => {
-
-        console.log(`New socket connected (${socket.id}) -> ${socket.email}`);
-
-        socket.on('cmd', (device_id, action) => {
-            const validDevice = devices.get(socket.uid)?.get(device_id); // ? - hack ;D
-            if (validDevice) socket.to(device_id).emit(action);
-        });
-
-        socket.on('disconnect', reason => {
-            console.log(`Socket disconnected (${socket.id}) -> ${reason}`);
-            if (socket.isDevice) {
-                io.to(socket.uid).emit('device disconnected', socket.id, reason);
-                devices.get(socket.uid).delete(socket.id);
-            }
-        });
-
-    });
+    io.on('connection', on_connection);
 
 }
